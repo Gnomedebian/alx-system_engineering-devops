3a30e3eb979d62c48a9128bd7b4fd3fb18065f94
Yassine Amgarou <90642482+Gnomedebian@users.noreply.github.com>
2024-01-08 22:34:53 +0100

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,7 +0,0 @@
-# Install flask using puppet
-package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3', }
-package { 'werkzeug':
-    ensure   => '2.1.1',
-    provider => 'pip3', }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,4 +0,0 @@
-# creating a manifest that kills a process: killmenow
-exec { 'killmenow' :
-    path    => '/bin/',
-    command => 'pkill killmenow', }
