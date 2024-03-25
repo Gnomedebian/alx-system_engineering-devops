300ee17bdeb7c98570ed175c21303006b2817756
Gnomedebian <yassineamgarou@gmail.com>
2024-03-25 22:03:32 +0000

    task::data-from-api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Py script that use REST API for a given employee ID
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    BASE_URL = 'https://jsonplaceholder.typicode.com'
+    employee_id = sys.argv[1]
+    response = requests.get('{}/todos?userId={}'.format(BASE_URL, employee_id))
+    todos = response.json()
+
+    response_user = requests.get('{}/users/{}'.format(BASE_URL, employee_id))
+    user_info = response_user.json()
+    employee_name = user_info['name']
+
+    completed_tasks = [todo for todo in todos if todo['completed']]
+    num_completed_tasks = len(completed_tasks)
+    total_tasks = len(todos)
+
+    print("Employee {} is done with tasks({}/{}):".format(
+            employee_name, num_completed_tasks, total_tasks))
+
+    for task in completed_tasks:
+        print("\t {}".format(task['title']))
