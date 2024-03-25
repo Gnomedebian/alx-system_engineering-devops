8b885bcb19956a6d90e3ed921576df3df9a96613
Gnomedebian <yassineamgarou@gmail.com>
2024-03-25 13:53:19 +0000

    task::json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,32 +7,19 @@ and exports it in CSV format.
 
 import json
 import requests
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-
-    BASE_URL = 'https://jsonplaceholder.typicode.com'
-    employee_id = sys.argv[1]
-    response = requests.get('{}/todos?userId={}'.format(
-                            BASE_URL, employee_id))
-    todos = response.json()
-
-    response_user = requests.get('{}/users/{}'.format(
-                            BASE_URL, employee_id))
-    user_info = response_user.json()
-    employee_name = user_info['name']
-
-    data = {employee_id: []}
-
-    for todo in todos:
-        task_info = {
-                'task': todo['title'],
-                'completed': todo['completed'],
-                'username': employee_name
-                }
-        data[employee_id].append(task_info)
-
-    filename = '{}.json'.format(employee_id)
-
-    with open(filename, 'w') as json_file:
-        json.dump(data, json_file)
+if __name__ == '__main__':
+    uid = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
+    user = requests.get(url, verify=False).json()
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(uid)
+    todo = requests.get(url, verify=False).json()
+    name = user.get('username')
+    t = [{"task": t.get("title"),
+          "username": name,
+          "completed": t.get("completed")} for t in todo]
+    bj = {}
+    bj[uid] = t
+    with open("{}.json".format(uid), 'w') as filejs:
+        json.dump(bj, filejs)
