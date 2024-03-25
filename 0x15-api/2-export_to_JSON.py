cd78d9dee5b7580a74571402625c4a4145e51f79
Gnomedebian <yassineamgarou@gmail.com>
2024-03-25 14:08:27 +0000

    task::json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,48 +1,31 @@
 #!/usr/bin/python3
-"""
-give employee ID, returns infos about todo list progress
-and exports it in JSON format.
-"""
-
-import json
+'''
+script gathers data from an API and exports it as an CSV file
+'''
+import re
 import requests
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <user_id>".format(sys.argv[0]))
-        sys.exit(1)
 
-    user_id = sys.argv[1]
+API_URL = 'https://jsonplaceholder.typicode.com'
+'''API url'''
 
-    try:
-        url = "https://jsonplaceholder.typicode.com/"
-        user_response = requests.get(url + "users/{}".format(user_id))
-        user_response.raise_for_status()
-        user = user_response.json()
-        username = user.get("username")
 
-        todos_response = requests.get(url + "todos", param={"userId": user_id})
-        todos_response.raise_for_status()
-        todos = todos_response.json()
-
-        with open("{}.json".format(user_id), "w") as jsonfile:
-            json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
-
-        print("Data exported successfully to {}.json".format(user_id))
-
-    except requests.exceptions.HTTPError as err:
-        print("HTTP Error:", err)
-        sys.exit(1)
-
-    except json.JSONDecodeError as err:
-        print("JSON Decoding Error:", err)
-        sys.exit(1)
-
-    except Exception as e:
-        print("An error occurred:", e)
-        sys.exit(1)
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
+            todos_res = requests.get('{}/todos'.format(API_URL)).json()
+            user_name = user_res.get('username')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            with open('{}.csv'.format(id), 'w') as file:
+                for todo in todos:
+                    file.write(
+                        '"{}","{}","{}","{}"\n'.format(
+                            id,
+                            user_name,
+                            todo.get('completed'),
+                            todo.get('title')
+                        )
+                    )
