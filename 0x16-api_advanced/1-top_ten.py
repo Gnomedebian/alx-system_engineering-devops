ec4cfd6030ee0423b75cd55c0bb081fc4c30e15b
Gnomedebian <yassineamgarou@gmail.com>
2024-04-09 17:19:00 +0000

    task::reddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+prints the titles of the first 10 hot posts
+"""
+
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'user agent 1.0'}
+    response = requests.get(url, headers=headers,
+                            allow_redirects=False)
+    if response.status_code == 200:
+        children = response.json().get('data').get('children')
+        for i in range(10):
+            print(children[i].get('data').get('title'))
+    else:
+        print("None")
