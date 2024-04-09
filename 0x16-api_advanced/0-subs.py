83293c130f73ddcd32d51e7ceab3a1d3ea0a2eea
Gnomedebian <yassineamgarou@gmail.com>
2024-04-09 17:47:20 +0000

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""queries the Reddit API returns number of subscribers for given subreddit"""
+"""
+Script queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that return the number of subscribers
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Curious_Salt3631)"
-    }
-    response = requests.get(url, headers=headers,
-                            allow_redirects=False)
+    """Return total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return data.get('data').get('subscribers')
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
